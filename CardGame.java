2d7bf5fb5d882d4b5ac0fc79cd9c27adf22444e0
Charlie Goldstraw <goldstrawcharlie@gmail.com>
2022-10-27 11:18:37 +0100

    Number of player input received

---
+++ CardGame.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+class CardGame {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numPlayers = 0;
+        while (numPlayers <= 0) {
+            System.out.println("Please enter the number of players.");
+            String numPlayerInput = scan.nextLine();
+            try {
+                numPlayers = Integer.parseInt(numPlayerInput);
+                if (numPlayers > 0) {
+                    break;
+                }
+                System.out.println("Please enter a positive number of players.");
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number of players.");
+            }
+        }
+        scan.close();
+    }
+}
