51bf6507e5451c8b8af194d8f2fa31debf67e7ca
Charlie Goldstraw <goldstrawcharlie@gmail.com>
2022-11-16 11:07:20 +0000

    Fixed rare testing error

--- src/CardGameTest.java
+++ src/CardGameTest.java
@@ -48,7 +48,7 @@ public class CardGameTest {
         // Systems Testing for the entire game. Creates a mock pack file and ensures the program
         // runs without throwing an error.
         Random rand = new Random();
-        int numPlayers = rand.nextInt(10);
+        int numPlayers = rand.nextInt(6,10);
         String packStr = "";
         for (int i = 0; i < 4*numPlayers; i++) {
             packStr += (int)(i/4)+1 + "\n";
